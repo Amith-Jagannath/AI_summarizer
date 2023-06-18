80c65583a2a1f42aa2313d383550fa9a12659fb1
Amith-Jagannath <amithjagannath02@gmail.com>
2023-06-18 16:37:33 +0530

    added demo section to render fetched data

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,15 +6,36 @@ const Demo = () => {
     url: "",
     summary: "",
   });
+  const [allArticle, setAllArticle] = useState([]);
+
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("article")
+    );
+    if (articlesFromLocalStorage) {
+      setAllArticle(articlesFromLocalStorage);
+    }
+  }, []);
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = await getSummary({ articleUrl: article.url });
     if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
       setArticle(newArticle);
+      const updatedAllArticle = [newArticle, ...allArticle];
+      setAllArticle(updatedAllArticle);
+      localStorage.setItem("article", JSON.stringify(updatedAllArticle));
       console.log(newArticle);
     }
+  };
+  const [copied, setCopied] = useState([]);
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => {
+      setCopied([]);
+    }, 4000);
   };
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -41,9 +62,54 @@ const Demo = () => {
             className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
             type="submit"
           >
-            🔄
+            ✅
           </button>
         </form>
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticle.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copied == item.url ? tick : copy}
+                  alt="copy"
+                  className="w-[40%] h-[60%] object-contain"
+                  onClick={() => handleCopy(item.url)}
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-meduim text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black text-center">
+            well,that wasnt supposed to happen
+            <br />
+            <span>{error?.data?.error}</span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2>
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium  text-gray">
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
       </div>
     </section>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,10 @@ const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        {/* <img src={logo} alt="logo" className="w-28 object-contain" /> */}
+        <h1 className="w-28 object-contain text-2xl font-bold logo-text">
+          SUMMARIZER
+        </h1>
         <button
           type="button"
           className="black_btn"
@@ -15,11 +18,11 @@ const Hero = () => {
       </nav>
       <h1 className="head_text">
         Summarize Article with <br className="max-md:hidden" />
-        <span className="orange_gradient">OpenAL GPT-4</span>
+        <span className="purple_gradient">Rapid API</span>
       </h1>
-      <h2 className="desc">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias eaque
-        molestiae ullam sint consequatur at dolore numquam et obcaecati,
+      <h2 className="desc ">
+        Simplify your reading with summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
   );
